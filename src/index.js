353be0fba31d473f1f5eee2f391411793ec4c50a
thallenk <thallen_k@hotmail.com>
2021-09-11 16:45:10 -0300

    create HomeView

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HomeView from './views/Home.view'
 
-function App () {
-  return<div>
-    Olá, mundo
-  </div>
-}
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomeView />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/views/Home.view.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeView () {
+    return(
+        <div>
+            Olá
+        </div>
+    )
+}
+
+export default HomeView;
