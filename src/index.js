0cb5b03295c21960cd90ca381241a916e4174304
thallenk <thallen_k@hotmail.com>
2021-09-11 16:36:49 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function App () {
+  return<div>
+    Olá, mundo
+  </div>
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,4 +17,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
